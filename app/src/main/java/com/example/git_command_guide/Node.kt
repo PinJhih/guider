1ea408f859ae8311994ff082296bf076f298bc70
PinJhih <isaacwang29@gmail.com>
2020-08-17 20:00:37 +0800

    feat: 使用 data class 儲存每個 item 的資料

---
+++ app/src/main/java/com/example/git_command_guide/Node.kt
@@ -0,0 +1,7 @@
+package com.example.git_command_guide
+
+data class Node(
+    var options: ArrayList<String> = ArrayList(),
+    var nextId: ArrayList<Int> = ArrayList(),
+    var position: Int = 0
+)
